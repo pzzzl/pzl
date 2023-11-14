26ebdb60aba921c8db811e6434d6371e25a0025b
Bruno Peselli <brunopeselli@gmail.com>
2023-11-14 02:13:11 -0300

    Major updates

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-const hideCarouselOnMobile = false;
-const carousel = document.getElementById("mainCarousel");
-
 function displayFunnyMessage() {
   const styledMessage = "%cCaution! Mischievous robots may be on the loose! 🤖";
   const styles = "color: red; font-size: 18px;";
   console.log(styledMessage, styles);
 }
 
-function hideCarousel(hide = hideCarouselOnMobile) {
-  if (hide) {
-    carousel.classList.add("d-none", "d-sm-block");
-  }
-}
-
 displayFunnyMessage();
-hideCarousel();
 
 $(document).ready(function () {
-  // Fecha a navegação ao clicar em um link
   $(".nav-link").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
